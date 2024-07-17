c8fec9b33ce852cd9307bff3292d8bf74ea4067b
Keerthana <dskithu07@gmail.com>
2024-07-17 15:38:19 +0530

    Added main

---
+++ main.lua
@@ -0,0 +1,21 @@
+function love.load()
+    loadModule("home/main.lua")
+end
+
+function loadModule(module)
+    if love.unload then love.unload() end
+    love.filesystem.load(module)()
+    if love.load then love.load() end
+end
+
+function goToHomePage()
+    loadModule("home/main.lua")
+end
+
+function goToScoreboard()
+    loadModule("score_board/main.lua")
+end
+
+function goToGamePage()
+    loadModule("game/game.lua")
+end
